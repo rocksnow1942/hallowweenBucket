175e27ade879ad0c3ea5960e03190fbdfddabd41
rocksnow1942 <rocksnow1942@gmail.com>
2021-10-31 10:40:42 -0700

    ON: Sun Oct 31 10:40:40 2021, FROM: huis-mac-pro.lan

--- ledControl.py
+++ ledControl.py
@@ -8,6 +8,7 @@ import adafruit_tlc59711
 class LEDControl(Thread,Logger):
     def __init__(self,main):
         self.main = main
+        super().__init__()
         Logger.__init__(self,'LED',fileHandler = self.main.fileHandler)
         spi = busio.SPI(board.SCK, MOSI=board.MOSI)
         self.pixels = adafruit_tlc59711.TLC59711(spi, pixel_count=12)
